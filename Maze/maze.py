0b3fdb08eddd75d32f861816662a555a31e3df44
keithnull <keith1126@126.com>
2019-10-15 23:11:34 +0800

    Implement BFS

--- Maze/maze.py
+++ Maze/maze.py
@@ -4,12 +4,22 @@ import matplotlib.patches as mpathes
 
 
 class MazeProblem:
+
     def __init__(self, maze_file=''):
-        self.map = self.loadMap(maze_file)
+        self.init_map = self.loadMap(maze_file)
+        self.map = self.init_map.copy()
+        self.m, self.n = self.map.shape
+        self.algorithms = {
+            "bfs": self.__BFS,
+            "a star": self.__A_star,
+            "a*": self.__A_star,
+        }
+        self.min_dist = float("inf")
 
     def loadMap(self, file):
         # Load map txt as matrix.
         # 0: path, 1: obstacle, 2: start point, 3: end point
+        # 9: solution
         f = open(file)
         lines = f.readlines()
         numOfLines = len(lines)
@@ -25,6 +35,7 @@ class MazeProblem:
     def drawMap(self):
         # Visulize the maze map.
         # Draw obstacles(1) as red rectangles. Draw path(0) as white rectangles. Draw starting point(2) and ending point(3) as circles.
+        # Draw solution(9) as blue rectangles.
         rowNum = len(self.map)
         print(rowNum)
         colNum = len(self.map[0])
@@ -42,6 +53,9 @@ class MazeProblem:
                 elif self.map[row, col] == 1:
                     rect = mpathes.Rectangle([param * col, param * row], param, param, color='r')
                     ax.add_patch(rect)
+                elif self.map[row, col] == 9:
+                    rect = mpathes.Rectangle([param * col, param * row], param, param, color='b')
+                    ax.add_patch(rect)
                 else:
                     rect = mpathes.Rectangle([param * col, param * row], param, param, color='w')
                     ax.add_patch(rect)
@@ -57,9 +71,57 @@ class MazeProblem:
         axx.invert_yaxis()
         plt.grid()
         # Save maze image.
-        plt.savefig('./maze.png')
+        plt.show()
+        # plt.savefig('./maze.png')
+
+    def __next_grid(self, s):
+        # return all possible new states for state s
+        x, y = s
+        if x != 0 and self.map[x - 1, y] != 1:
+            yield (x - 1, y)
+        if x != self.m - 1 and self.map[x + 1, y] != 1:
+            yield (x + 1, y)
+        if y != 0 and self.map[x, y - 1] != 1:
+            yield (x, y - 1)
+        if y != self.n - 1 and self.map[x, y + 1] != 1:
+            yield (x, y + 1)
+
+    def __is_end(self, s):
+        return s == (self.m - 1, self.n - 1)
+
+    def __BFS(self):
+        layer = [(0, 0)]
+        parent = {(0, 0): None}  # visited
+        dist = 0
+        while layer:
+            next_layer = []
+            for grid in layer:
+                if self.__is_end(grid):
+                    x = grid
+                    while parent[x]:
+                        self.map[x] = 9
+                        x = parent[x]
+                    self.min_dist = min(dist, self.min_dist)
+                    return dist
+                for new_grid in self.__next_grid(grid):
+                    if new_grid in parent.keys():
+                        continue
+                    parent[new_grid] = grid
+                    next_layer.append(new_grid)
+            dist += 1
+            layer = next_layer
+        # if no solution found
+        return None
+
+    def solve(self, algorithm="BFS"):
+        self.map = self.init_map.copy()
+        algorithm = algorithm.lower()
+        if algorithm not in self.algorithms.keys():
+            raise Exception("Unsupported algorithm: " + algorithm)
+        print("Shortest distance:", self.algorithms[algorithm]())
 
 
 if __name__ == "__main__":
     Solution = MazeProblem(maze_file='maze.txt')
+    Solution.solve("BFS")
     Solution.drawMap()
